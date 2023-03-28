b87609409a07091a07305602595899bcc24408bd
benyy0101 <65917479+benyy0101@users.noreply.github.com>
2023-03-28 18:26:25 +0900

    [unrated] Title: 카드 뭉치, Time: 0.01 ms, Memory: 9.94 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+def solution(cards1, cards2, goal):
+    answer = ''
+    goal = deque(goal)
+    cards1 = deque(cards1)
+    cards2 = deque(cards2)
+    
+    while goal:
+        target = goal.popleft()
+        
+        if cards1 and target == cards1[0]:
+            cards1.popleft()
+        elif cards2 and target == cards2[0]:
+            cards2.popleft()
+        else:
+            return 'No'
+    return 'Yes'
