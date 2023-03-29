d7e0a55a6bee1510e93429eabd863f4f7639484b
benyy0101 <65917479+benyy0101@users.noreply.github.com>
2023-03-29 10:43:29 +0900

    [unrated] Title: 과일 장수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -0,0 +1,9 @@
+def solution(k, m, score):
+    answer = 0
+    score.sort(reverse = True)
+    
+    for i in range(0,len(score),m):
+        if (i+m) <= len(score):
+            answer += m * min(score[i:i+m])
+    
+    return answer
