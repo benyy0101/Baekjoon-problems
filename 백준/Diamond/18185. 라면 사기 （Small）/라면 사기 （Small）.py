2e02a0ada0d5fb0d647a499f65f73a04dbe8a943
benyy0101 <65917479+benyy0101@users.noreply.github.com>
2023-05-15 15:02:21 +0900

    [Diamond V] Title: 라면 사기 (Small), Time: 60 ms, Memory: 32276 KB -BaekjoonHub

---
+++ 백준/Diamond/18185. 라면 사기 （Small）/라면 사기 （Small）.py
@@ -0,0 +1,35 @@
+import sys
+
+N = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().split()))
+answer = 0
+
+for i in range(N-2):
+    if A[i+1] > A[i+2]:
+        m = min(A[i], A[i+1] - A[i+2])
+        answer += m * 5
+        A[i] -= m
+        A[i+1] -= m
+    
+    if A[i] > 0 and A[i+1] > 0 and A[i+2] > 0:
+        m = min(A[i], A[i+1])
+        answer += m * 7
+        A[i] -= m
+        A[i+1] -= m
+        A[i+2] -= m
+    
+    if A[i] > 0:
+        answer += A[i] * 3
+                
+if A[-2] > 0 and A[-1] > 0:
+    m = min(A[-2], A[-1])
+    answer += (m * 5)
+    A[-2] -= m
+    A[-1] -= m
+
+if A[-2] > 0:
+    answer += (A[-2] * 3) 
+else:
+    answer += (A[-1] * 3)
+
+print(answer)
