4aac02f2a62c7a8189989f1c771842ef6bdd6b13
benyy0101 <65917479+benyy0101@users.noreply.github.com>
2023-08-18 15:34:55 +0900

    [Gold IV] Title: 탈출, Time: 84 ms, Memory: 11760 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -1,126 +1,88 @@
-import java.util.*;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int R, C;
-	static char map[][];
-	
-	static Point start, end;
-	
-	static class Point{
-		int r; int c;
-
-		public Point(int r, int c) {
-			super();
-			this.r = r;
-			this.c = c;
-		}
-		
-	}
-	
-	static int[] dr = {-1, 0, 1, 0};
-	static int[] dc = {0, -1, 0, 1};
-	
-	public static void main(String[] args) throws Exception {
+	static int direction[][] = new int[][] {
+		{1,0},
+		{0,1},
+		{-1,0},
+		{0,-1}
+	};
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		
-		map = new char[R][C];
-		Queue<Point> water = new LinkedList<>();
-		
-		for(int r=0; r<R; ++r) {
-			String str = br.readLine();
-			for(int c=0; c<C; ++c) {
-				map[r][c] = str.charAt(c);
-			
-				if(map[r][c]=='*') {
-					water.add(new Point(r,c));
-					continue;
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		char[][] map = new char[n][m];
+		int[] goal = new int[2];
+		Queue<int[]> qq = new ArrayDeque();
+		Queue<int[]> ww = new ArrayDeque();
+		boolean[][] v = new boolean[n][m];
+		for(int i = 0; i<n;i++) {
+			String ss = br.readLine();
+			for(int j = 0; j <m;j++) {
+				map[i][j] = ss.charAt(j);
+				if(map[i][j] == 'S') {
+					qq.offer(new int[] {i,j});
 				}
-				
-				if(map[r][c]=='S') {
-					start = new Point(r,c);
-					continue;
+				if(map[i][j] == '*') {
+					ww.offer(new int[] {i,j});
 				}
-				
-				if(map[r][c]=='D') {
-					end = new Point(r,c);
-					continue;
+				if(map[i][j] == 'D') {
+					goal[0] = i;
+					goal[1] = j;
 				}
-				
-			
 			}
 		}
+		int cnt = 0;
+		int flag = 0;
 		
-		int time = 0;
-		Queue<Point> q = new LinkedList<>();
-		q.add(start);
-		map[start.r][start.c] = 'X';
-		
-		int size, i, p, nr, nc;
-		Point curr;
-		while(true) {
-			
-			size = water.size();
-			for(i=0; i<size; ++i) {
-				curr = water.poll();
-				
-				for(p=0; p<4; ++p) {
-					nr = curr.r + dr[p];
-					nc = curr.c + dc[p];
-				
-					if(!inRange(nr, nc)) continue;
-				
-					if(map[nr][nc] == '.') {
-						water.add(new Point(nr, nc));
-						map[nr][nc] = '*';
-					}
+		top:
+		while(!qq.isEmpty()) {
+			//물 먼저 확산
+			int water = ww.size();
+			for(int i = 0; i<water;i++) {
+				int[] pos = ww.poll();
+				for(int j = 0; j <4;j++) {
+					int nx = pos[0] + direction[j][0];
+					int ny = pos[1] + direction[j][1];
+					if(nx < 0 || nx >= n || ny < 0|| ny >= m) continue;
+					if(map[nx][ny] != '.') continue;
+					map[nx][ny] = '*';
+					ww.offer(new int[] {nx,ny});
 				}
 			}
-			
-			size = q.size();
-			for(i=0; i<size; ++i) {
-				curr = q.poll();
-				
-				if(curr.r == end.r && curr.c == end.c) {
-					System.out.println(time);
-					System.exit(0);
+			int duck = qq.size();
+			cnt++;
+			for(int i = 0; i<duck;i++) {
+				int[] pos = qq.poll();
+				for(int j = 0; j <4;j++) {
+					int nx = pos[0] + direction[j][0];
+					int ny = pos[1] + direction[j][1];
+					if(nx < 0 || nx >= n || ny < 0|| ny >= m) continue;
+					if(v[nx][ny]) continue;
+					if(map[nx][ny] == 'D') {
+						flag = 1;
+						break top;
+					} 
+					if(map[nx][ny] != '.') continue;
+					
+					v[nx][ny] = true;
+					qq.offer(new int[] {nx,ny});
 				}
-				
-				for(p=0; p<4; ++p) {
-					nr = curr.r + dr[p];
-					nc = curr.c + dc[p];
-				
-					if(!inRange(nr, nc)) continue;
-				
-					if(map[nr][nc] == '.' || map[nr][nc]=='D') {
-						q.add(new Point(nr, nc));
-						map[nr][nc] = 'X';
-					}
-				}
-			}
-			
-			++time;
-			if(q.isEmpty()) {
-				System.out.println("KAKTUS");
-				System.exit(0);
-			}
-			
-			
+			}			
 		}
 		
-
+		if(flag == 1) {
+			System.out.println(cnt);
+		}
+		else {
+			System.out.println("KAKTUS");
+		}
 	}
-	
-	static boolean inRange(int r, int c) {
-		if(r < 0 || c < 0 || r >= R || c >= C) return false;
-		return true;
-	}
-
 }
