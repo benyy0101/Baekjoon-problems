9e65505ded5bfa4b222c97629406edcc18862a89
benyy0101 <65917479+benyy0101@users.noreply.github.com>
2023-08-25 10:32:57 +0900

    [Gold IV] Title: 회전 초밥, Time: 548 ms, Memory: 180352 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.java
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -1,54 +1,53 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class Main  {
-    static int n, d, k, c;
-    static int[] sushi;
-    static int[] s;
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String [] str = br.readLine().split(" ");
-
-        n = Integer.parseInt(str[0]);
-        d = Integer.parseInt(str[1]);
-        k = Integer.parseInt(str[2]);
-        c = Integer.parseInt(str[3]);
-
-        sushi = new int[n*2];
-        s = new int[d+1];
-
-        for (int i = 0; i < n; i++) {
-            sushi[i] = Integer.parseInt(br.readLine());
-            sushi[n+i] = sushi[i];
-        }
-        s[c] += 1;
-
-        int count = 1;
-        int ans = 0;
-        for (int i = 0; i < k; i++) {
-            if (s[sushi[i]]  == 0) count += 1;
-            s[sushi[i]] += 1;
-        }
-        ans = Math.max(ans, count);
-
-        int start = 0, end = k;
-
-        for (int i = 1; i <= n; i++) {
-            if (s[sushi[start]] == 1) count -= 1;
-            s[sushi[start]] -= 1;
-            start += 1;
-
-            
-            if (s[sushi[end]] == 0) count += 1;
-            s[sushi[end]] += 1;
-            end += 1;
-
-            ans = Math.max(ans, count);
-        }
-
-        System.out.println(ans);
-        
-
-    }
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int D = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		
+		int[] sushiBelt = new int[N*2];
+		int[] onPlate = new int[D+1];
+		int answer = 0;
+		int result = 0;
+		int i;
+		for(i = 0 ; i < N;i++) {
+			sushiBelt[i] = Integer.parseInt(br.readLine());
+		}
+		
+		for(int k = i;k<2*N;k++) {
+			sushiBelt[k] = sushiBelt[k-N];
+		}
+		
+		for(int j = 0; j <K;j++) {
+			if(onPlate[sushiBelt[j]] == 0) {
+				onPlate[sushiBelt[j]]++;
+				answer++;
+			}
+			else {
+				onPlate[sushiBelt[j]]++;
+			}
+		}
+		
+		result = answer;
+		for(int j = 0; j <N;j++) {
+			onPlate[sushiBelt[j]]--;
+			if(onPlate[sushiBelt[j]] == 0) answer--;
+			onPlate[sushiBelt[K+j]]++;
+			if(onPlate[sushiBelt[K+j]] == 1) answer++;
+			if(onPlate[C] == 0) result = Math.max(result, answer+1);
+			else result = Math.max(result, answer);
+			
+		}
+		System.out.println(result);
+	}
 }
