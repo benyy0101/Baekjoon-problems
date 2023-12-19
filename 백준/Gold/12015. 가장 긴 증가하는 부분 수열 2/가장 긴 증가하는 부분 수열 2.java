c1357bf3bdb4a4a46da168f627222914459ea9da
benyy0101 <65917479+benyy0101@users.noreply.github.com>
2023-12-19 13:11:21 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 480 ms, Memory: 125332 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		int[] list = new int[N];
+		int[] lis = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0 ; i < N;i++) {
+			list[i] = Integer.parseInt(st.nextToken());
+		}
+		lis[0] = list[0];
+		int idx = 1;
+		for(int i = 0 ; i < N;i++) {
+			int current = list[i];
+			if(lis[idx-1] < current) {
+				idx++;
+				lis[idx-1] = current;
+			}
+			//binary search
+			else {
+				int left = 0;
+				int right = idx;
+				
+				while(left< right) {
+					int mid = (right-left)/2 + left;
+					//void result = lis[mid] < current ? left = mid+1 : right = mid;
+					
+					if(lis[mid] < current) left = mid +1;
+					else right = mid;					
+				}
+				
+				lis[left] = current;
+			}
+		}
+		System.out.println(idx);
+		
+	}
+}
