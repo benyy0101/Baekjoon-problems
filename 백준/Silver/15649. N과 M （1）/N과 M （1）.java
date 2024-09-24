acf145efba99f1f725d0c3bf5476029ff130606a
benyy0101 <65917479+benyy0101@users.noreply.github.com>
2024-09-24 22:12:21 +0900

    [Silver III] Title: N과 M (1), Time: 904 ms, Memory: 55068 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,60 +1,41 @@
-
-
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int[] nums;
-	static int depth;
-	static int length;
-	static boolean[] visited;
-	public static void permute(int i) {
-		if(i == depth) {
+	static int N, M;
+	static int[] arr;
+	static boolean[] v;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[M];
+		v = new boolean[N];
+		
+		dfs(0);
+	}
+	
+	public static void dfs(int depth) {
+		if(depth == M) {
 			StringBuilder sb = new StringBuilder();
-			
-			for(int item: nums) {
-				sb.append(item);
-				sb.append(" ");
+			for(int i:arr) {
+				sb.append(i).append(" ");
 			}
-			System.out.println(sb.toString());
+			System.out.println(sb);
 			return;
 		}
 		
-		for(int j = 1; j <= length; j++) {
-			if(visited[j] == true) continue;
-			nums[i] = j;
-			visited[j] = true;
-			permute(i + 1);
-			visited[j] = false;
+		for(int i = 0 ; i < N;i++) {
+			if(v[i]) continue;
+			v[i] = true;
+			arr[depth] = i+1;
+			dfs(depth+1);
+			v[i] = false;
 		}
-	}
-	
-	public static void permute(int i, int flag) {
-		if(i == depth) {
-			StringBuilder sb = new StringBuilder();
-			
-			for(int item: nums) {
-				sb.append(item);
-				sb.append(" ");
-			}
-			System.out.println(sb.toString());
-			return;
-		}
-		
-		for(int j = 0; j < length; j++) {
-			if((flag & 1 << j) != 0) continue;
-			nums[i] = j + 1;
-			permute(i+1, flag | 1<<j);
-		}
-	}
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		length = sc.nextInt();
-		depth = sc.nextInt();
-		nums = new int[depth];
-		visited = new boolean[length+1];
-		
-		permute(0);
 	}
 }
